2c921fe4dc78ebdd89aebbd855e2783d5b13e9e6
00IMPERA <nelutzyou@gmail.com>
2026-04-17 15:59:41 +0200

    Initialize main.jsx with React rendering setup

---
+++ main.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import WinnowinPage from "./Winnowin.jsx";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <WinnowinPage />
+  </React.StrictMode>
+);
